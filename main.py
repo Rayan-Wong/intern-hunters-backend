2c5e1455ba12692b99d21a9c5b2490e8e6c6def9
Rayan Wong <work.rayanwong@gmail.com>
2025-05-06 17:29:22 +0800

    Cleaned up tests

--- main.py
+++ main.py
@@ -4,19 +4,12 @@ from functools import lru_cache
 from fastapi import FastAPI, Depends, APIRouter
 from fastapi.security import OAuth2PasswordBearer
 
-from config import Settings
-
 import jwt
 from pydantic import BaseModel
 
 router = APIRouter(prefix="/api")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-@lru_cache
-def get_settings():
-    return Settings()
-
 
 @router.get("/")
 def read_root():
@@ -30,12 +23,6 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @router.get("/token/")
 async def read_item(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"token": token}
-
-@router.get("/info")
-async def info(settings: Annotated[Settings, Depends(get_settings)]):
-    return {
-        "lorem": settings.lorem
-    }
 
 app = FastAPI()
 app.include_router(router)
