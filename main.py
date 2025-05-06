a41e6b247042835191bc7b4080984a5c695cfa00
Rayan Wong <work.rayanwong@gmail.com>
2025-05-06 17:09:36 +0800

    Adding skeleton structure

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-from typing import Union
+from typing import Union, Annotated
+from functools import lru_cache
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, APIRouter
+from fastapi.security import OAuth2PasswordBearer
 
-app = FastAPI()
+from config import Settings
+
+import jwt
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/api")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@lru_cache
+def get_settings():
+    return Settings()
 
 
-@app.get("/")
+@router.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@router.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@router.get("/token/")
+async def read_item(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
+
+@router.get("/info")
+async def info(settings: Annotated[Settings, Depends(get_settings)]):
+    return {
+        "lorem": settings.lorem
+    }
+
+app = FastAPI()
+app.include_router(router)
