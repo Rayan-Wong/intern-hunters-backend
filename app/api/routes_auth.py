1f879fb2dc9c19ecf54b12b58bfeb1c46b702bed
Rayan Wong <work.rayanwong@gmail.com>
2025-05-08 12:20:32 +0800

    Cleanup of HTTP reponse codes

--- app/api/routes_auth.py
+++ app/api/routes_auth.py
@@ -15,9 +15,9 @@ def register_user(current_user: UserCreate):
         user = register(current_user)
         return Response(status_code=status.HTTP_201_CREATED)
     except DuplicateEmailError:
-        raise HTTPException(status_code=400, detail="Email already exists")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
     except:
-        raise HTTPException(status_code=500, detail="Something wrong")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something wrong")
 
 @router.post("/login")
 def login_user(user: UserLogin):
@@ -29,4 +29,4 @@ def login_user(user: UserLogin):
     except WrongPassword:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Password incorrect")
     except:
-        raise HTTPException(status_code=500, detail="Something wrong")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something wrong")
