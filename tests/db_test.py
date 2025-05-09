c14dfd81033ec7b4e62e850fb18710ec374778f0
Rayan Wong <work.rayanwong@gmail.com>
2025-05-09 09:38:05 +0800

    Updated test suite for login Note: will create tests branch to update test suite from now on

--- tests/db_test.py
+++ tests/db_test.py
@@ -16,3 +16,36 @@ def test_register():
         }
     )
     assert response.status_code == status.HTTP_201_CREATED
+
+def test_register_duplicate():
+    response = client.post("/api/register",
+        json={"name": "admin", 
+            "email": "urmum@gmail.com",
+            "password": "password"
+        }
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+def test_login():
+    response = client.post("/api/login",
+        json={"email": "urmum@gmail.com",
+            "password": "password"
+        }
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+def test_no_account_login():
+    response = client.post("/api/login",
+        json={"email": "urdad@gmail.com",
+            "password": "password"
+        }
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+def test_wrong_account_login():
+    response = client.post("/api/login",
+        json={"email": "urmum@gmail.com",
+            "password": "pasword"
+        }
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
