53621345b94467a903006ffedec93ca44f728c7f
Rayan Wong <work.rayanwong@gmail.com>
2025-05-09 18:57:48 +0800

    Refactored auth service to be a class for dependency injection

--- app/api/routes_auth.py
+++ app/api/routes_auth.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, status
-from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
-from app.services.auth_service import register, login
+from app.services.auth_service import UserAuth
 from app.schemas.user import UserCreate, UserLogin
 from app.exceptions.db_exceptions import DuplicateEmailError, NoAccountError, WrongPasswordError
+
+from typing import Annotated
 
 import jwt
 
 router = APIRouter(prefix="/api")
 
 @router.post("/register")
-def register_user(current_user: UserCreate):
+def register_user(current_user: UserCreate, auth: Annotated[UserAuth, Depends(UserAuth)]):
     try:
-        user = register(current_user)
+        user = auth.register(current_user)
         return Response(status_code=status.HTTP_201_CREATED)
     except DuplicateEmailError:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
@@ -20,9 +20,9 @@ def register_user(current_user: UserCreate):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something wrong")
 
 @router.post("/login")
-def login_user(user: UserLogin):
+def login_user(user: UserLogin, auth: Annotated[UserAuth, Depends(UserAuth)]):
     try:
-        user = login(user)
+        user = auth.login(user)
         return Response(status_code=status.HTTP_200_OK)
     except NoAccountError:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Account not created")
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -11,38 +11,39 @@ from app.models.user import User
 
 from app.exceptions.db_exceptions import DuplicateEmailError, WrongPasswordError, NoAccountError
 
-pwd_context = PasswordHasher()
-
-def register(user_in: UserCreate):
-    hashed_password = pwd_context.hash(user_in.password)
-    user = User(
-        name=user_in.name,
-        email=user_in.email, 
-        encrypted_password=hashed_password
-    )
-    db = get_session()
-    try:
-        db.add(user)
-        db.commit()
-    except IntegrityError:
-        db.rollback()
-        raise DuplicateEmailError
-    except Exception as e:
-        print(e)
-        raise e
-    return user
-
-def login(user_in: UserLogin):
-    db = get_session()
-    try:
-        stmt = select(User).where(user_in.email == User.email)
-        user = db.execute(statement=stmt).scalar_one()
-        pwd_context.verify(user.encrypted_password, user_in.password)
-        return user_in
-    except VerificationError:
-        raise WrongPasswordError
-    except NoResultFound:
-        raise NoAccountError
-    except Exception as e:
-        print(e)
-        raise e
+class UserAuth:
+    def __init__(self):
+        self.__db = get_session()
+        self.__pwd_context = PasswordHasher()
+    
+    def register(self, user_in: UserCreate):
+        hashed_password = self.__pwd_context.hash(user_in.password)
+        user = User(
+            name=user_in.name,
+            email=user_in.email, 
+            encrypted_password=hashed_password
+        )
+        try:
+            self.__db.add(user)
+            self.__db.commit()
+        except IntegrityError:
+            self.__db.rollback()
+            raise DuplicateEmailError
+        except Exception as e:
+            print(e)
+            raise e
+        return user
+        
+    def login(self, user_in: UserLogin):
+        try:
+            stmt = select(User).where(user_in.email == User.email)
+            user = self.__db.execute(statement=stmt).scalar_one()
+            self.__pwd_context.verify(user.encrypted_password, user_in.password)
+            return user_in
+        except VerificationError:
+            raise WrongPasswordError
+        except NoResultFound:
+            raise NoAccountError
+        except Exception as e:
+            print(e)
+            raise e
