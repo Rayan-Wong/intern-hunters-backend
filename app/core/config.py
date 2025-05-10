de12a561531601114c2f0a5eda4dd7edcd0e56b4
Rayan Wong <work.rayanwong@gmail.com>
2025-05-10 09:59:12 +0800

    Hacky change to allow approprioate db usage if in prod or in pytests

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,11 @@ from pydantic import Field
 from dotenv import load_dotenv
 from pathlib import Path
 from functools import lru_cache
+import os, sys
 
-load_dotenv(dotenv_path=Path(__file__).resolve().parents[2] / ".env", override=False)
+RUNNING_PYTEST = "PYTEST_CURRENT_TEST" in os.environ or any("pytest" in p for p in sys.argv)
+
+load_dotenv(dotenv_path=Path(__file__).resolve().parents[2] / ".env", override=not RUNNING_PYTEST)
 
 class Settings(BaseSettings):
     database_url: str
