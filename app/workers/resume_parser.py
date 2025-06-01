79923a8b18d74cabeb973013dc7452b731484887
Rayan Wong <work.rayanwong@gmail.com>
2025-06-01 23:35:36 +0800

    Made resume skills parser async, polished initial setup to avoid delay

--- app/workers/resume_parser.py
+++ app/workers/resume_parser.py
@@ -1,23 +1,34 @@
 import json
 import os
+import asyncio
 
 import spacy
 import pymupdf
 
-nlp = spacy.load("en_core_web_sm")
+nlp = None
 skills_set = set()
 
 current_dir = os.path.dirname(__file__)
-json_path = os.path.join(current_dir, "programming_technologies.json")
-pdf_path = os.path.join(current_dir, "test_resume.pdf")
 
-with open(json_path, "r") as file:
-    dict_list = json.load(file)
-    for dict in dict_list:
-        skills_set.add(dict["name"].lower())
+def load_nlp():
+    global nlp
+    if nlp is None:
+        nlp = spacy.load("en_core_web_sm")
 
-def get_skills():
+def load_skills():
+    global skills_set
+    if not skills_set:
+        json_path = os.path.join(current_dir, "programming_technologies.json")
+        with open(json_path, "r") as file:
+            dict_list = json.load(file)
+            for dict in dict_list:
+                skills_set.add(dict["name"].lower())
+
+async def get_skills():
     """Retrieve skills from user's resume"""
+    load_nlp()
+    load_skills()
+    pdf_path = os.path.join(current_dir, "test_resume.pdf")
     doc = pymupdf.open(pdf_path)
     text = ""
     result = set()
@@ -32,4 +43,4 @@ def get_skills():
             result.add(chunk.text)
     return result
 
-print(get_skills())
+print(asyncio.run(get_skills()))
