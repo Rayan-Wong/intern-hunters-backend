9c6674f99e83e880402c72bcde89162205daed66
Rayan Wong <work.rayanwong@gmail.com>
2025-06-17 10:31:03 +0800

    Added base revision for alembic for both dev and prod db, improved layering of docker image for backend, remove duplicate unique constraint on user id

--- app/models/user.py
+++ app/models/user.py
@@ -9,7 +9,7 @@ from .base import Base
 class User(Base):
     """Model of how a user is stored"""
     __tablename__ = "user"
-    id: Mapped[uuid.UUID] = mapped_column(Uuid(), primary_key=True, default=uuid.uuid4, unique=True)
+    id: Mapped[uuid.UUID] = mapped_column(Uuid(), primary_key=True, default=uuid.uuid4)
     name: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
     encrypted_password: Mapped[str]
