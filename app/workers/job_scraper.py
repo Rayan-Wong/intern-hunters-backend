c70922118d3c9b8c49dbac6a0c0988a828dda490
Rayan Wong <work.rayanwong@gmail.com>
2025-07-12 17:01:41 +0800

    Fixed redis caching strategy, added timeout for job scraper, fixed test fixtures

--- app/workers/job_scraper.py
+++ app/workers/job_scraper.py
@@ -1,4 +1,6 @@
 """Modules responsible for jobspy and post-processing"""
+from concurrent.futures import ThreadPoolExecutor, TimeoutError
+
 from jobspy import scrape_jobs
 import numpy as np
 
@@ -8,6 +10,8 @@ from app.core.timer import timed
 from app.core.logger import setup_custom_logger
 
 logger = setup_custom_logger(__name__)
+
+TIMEOUT = 60
 
 columns = [
     "site",
@@ -32,17 +36,24 @@ def sync_scrape_jobs(preference: str, start: int, end: int, preferred_industry: 
         else:
             term = f"{preference} (intern OR internship OR co-op OR 'summer intern' OR 'summer analyst')"
         logger.info(f"Starting API call value: {start}, Ending API call value: {end}")
-        jobs = scrape_jobs(
-            site_name=["linkedin", "indeed"],
-            search_term=term,
-            location="Singapore",
-            results_wanted=end-start, # gross, to change,
-            hours_old=MAX_HOURS, # 3 weeks
-            offset=start,
-            country_indeed="Singapore",
-            linkedin_fetch_description=True,
-            description_format="html"
-        )
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            future = executor.submit(
+                scrape_jobs,
+                site_name=["linkedin"],
+                search_term=term,
+                location="Singapore",
+                results_wanted=end-start, # gross, to change,
+                hours_old=MAX_HOURS, # 3 weeks
+                offset=start,
+                country_indeed="Singapore",
+                linkedin_fetch_description=True,
+                description_format="html"
+            )
+            try:
+                jobs = future.result(timeout=TIMEOUT)
+            except TimeoutError:
+                logger.error("Job Scraper API took too long to respond!")
+                raise ScraperDown from TimeoutError
         if jobs.empty:
             return [] # todo: decide what to do if no results
         result = jobs[columns].replace({np.nan: None}) # thanks numpy
